ddbd6737e98fcd371d3e6df2842684de84f3d123
Krunal-Parmar <100572810+Krunal-123@users.noreply.github.com>
2024-10-21 20:29:16 +0530

    update 1.5

--- src/ExtraCompo.jsx
+++ src/ExtraCompo.jsx
@@ -3,9 +3,22 @@ import Footer from '../components/Footer.jsx'
 import MobileOnly from '../components/MobileOnly.jsx'
 import { Outlet} from 'react-router-dom';
 import { useCart } from './context/CartContext.jsx';
+import { useCookies } from 'react-cookie';
+import { Outlet, useNavigate } from 'react-router-dom';
+import { useEffect } from 'react';
 
 export default function (){
     const {LightMode}=useCart()
+    let Navigate = useNavigate()
+    const [cookies] = useCookies()
+    useEffect(() => {
+        if (!cookies.token) {
+            Navigate('/login')
+        }
+        else {
+            Navigate('/home')
+        }
+    }, [])
     return (
         <>
         <div className={`${LightMode?"bg-slate-900":"bg-white"}`}>
--- src/HomePage.jsx
+++ src/HomePage.jsx
@@ -1,22 +1,11 @@
-import React, { useEffect } from 'react';
-import { useCookies } from 'react-cookie';
-import { Outlet, useNavigate } from 'react-router-dom';
+import React from 'react';
 import "../src/App.css"
 import { CartProvider } from '../src/context/CartContext.jsx'
 import ExtraCompo from './ExtraCompo.jsx';
 
 
 export default function () {
-    let Navigate = useNavigate()
-    const [cookies] = useCookies()
-    useEffect(() => {
-        if (!cookies.token) {
-            Navigate('/login')
-        }
-        else {
-            Navigate('/home')
-        }
-    }, [])
+
     return (
         <>
             <CartProvider>

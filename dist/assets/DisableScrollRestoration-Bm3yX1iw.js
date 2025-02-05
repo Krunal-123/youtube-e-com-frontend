aa1822097e99942dcfdb3c5eff295e44b2449b78
Krunal-Parmar <100572810+Krunal-123@users.noreply.github.com>
2025-02-06 01:11:11 +0530

    9.0

---
+++ dist/assets/DisableScrollRestoration-Bm3yX1iw.js
@@ -0,0 +1,1 @@
+import{r as o}from"./index-DBa0aW7u.js";const t=()=>o.useEffect(()=>{window.scrollTo(0,0)},[]);export{t as D};
--- dist/assets/DisableScrollRestoration-Cc52kSPE.js
+++
@@ -1,1 +0,0 @@
-import{r as o}from"./index-DPMa0iSw.js";const t=()=>(o.useEffect(()=>{window.scrollTo(0,0)},[]),null);export{t as D};
